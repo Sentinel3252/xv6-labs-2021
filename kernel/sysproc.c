1995760860ce1e0b051821d1f3654a30e3cf2c67
Junqi Guo <2313815959@qq.com>
2023-08-10 11:35:31 +0800

    Add files via upload

---
+++ kernel/sysproc.c
@@ -0,0 +1,140 @@
+#include "types.h"
+#include "riscv.h"
+#include "param.h"
+#include "defs.h"
+#include "date.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  if(argint(0, &n) < 0)
+    return -1;
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  if(argaddr(0, &p) < 0)
+    return -1;
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+
+#ifdef LAB_PGTBL
+int 
+sys_pgaccess(void)
+ {
+  uint64 a;
+  int num;
+  uint64 mask;
+  pte_t *pte;
+  char buf[512] = {0};
+  int len;
+
+  if (argaddr(0, &a) < 0)
+      return -1;
+  if (argint(1, &num) < 0)
+      return -1;
+  if (argaddr(2, &mask) < 0)
+      return -1;
+
+  uint64 start = PGROUNDDOWN(a);
+  for (int i = 0, va = start; i < num; i++, va += PGSIZE){
+      pte = walk(myproc()->pagetable, va, 0);
+      if (*pte & PTE_A) {
+          buf[i >> 3] |= (1 << (i & 0x07));
+          *pte = (*pte)&(~PTE_A);
+      } else {
+          buf[i >> 3] &= ~(1 << (i & 0x07));
+      }
+  }
+
+  len = (num + 7) / 8;
+
+  if (copyout(myproc()->pagetable, mask, buf, len) < 0) {
+      return -1;
+  }
+
+  return 0;
+ }
+#endif
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
